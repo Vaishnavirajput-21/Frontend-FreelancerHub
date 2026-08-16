ec39e6816add4b32ce03ad8b905577b9eee1d976
VaishnaviRajput-21 <vr813706@gmail.com>
2026-08-17 01:39:54 +0530

    Fix Vercel build errors

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,5 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 function NotFound() {
-  return <h1>404 - Page Not Found</h1>;
+  const navigate = useNavigate();
+
+  return (
+    <div style={{ textAlign: "center", padding: "80px" }}>
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+
+      <p>The page you are looking for does not exist.</p>
+
+      <button onClick={() => navigate("/")}>
+        Go Home
+      </button>
+    </div>
+  );
 }
 
 export default NotFound;
